4172731acf81cd28b289d33fc36063331a8ab86b
Shivananda Sahu <sshivananda@athenahealth.com>
2020-05-30 20:08:36 +0530

    Adding terraform to create sqs

---
+++ integration-test/localstack.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "fake"
+  secret_key                  = "fake"
+  skip_requesting_account_id  = true
+  skip_credentials_validation = true
+
+  endpoints {
+    sqs = "http://localhost:4566"
+  }
+}
+
+resource "aws_sqs_queue" "test_queue" {
+  name                              = "test_queue"
+  delay_seconds                     = 90
+  max_message_size                  = 2048
+  message_retention_seconds         = 86400
+  receive_wait_time_seconds         = 10
+  kms_master_key_id                 = "alias/aws/sqs"
+  kms_data_key_reuse_period_seconds = 300
+}
