c2e59a4acca797fda06d1d81fb3a85302f582e96
Shivananda Sahu <sshivananda@athenahealth.com>
2020-05-31 17:24:24 +0530

    Exiting if test fails

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -1,8 +1,43 @@
 cd integration-test
 TMPDIR=/private$TMPDIR docker-compose up -d
+if [ $? -eq 0 ]
+then
+  echo "Successfully started docker"
+else
+  echo "Could not start docker" >&2
+  exit $?
+fi
 terraform init
+if [ $? -eq 0 ]
+then
+  echo "Successfully started terraform"
+else
+  echo "Could not start terraform" >&2
+  exit $?
+fi
 terraform apply -auto-approve
+if [ $? -eq 0 ]
+then
+  echo "Successfully created resources"
+else
+  echo "Could not create resources" >&2
+  exit $?
+fi
 cd ..
 npm run integration-test
+if [ $? -eq 0 ]
+then
+  echo "Successfully ran integration test"
+else
+  echo "Failed integration test" >&2
+  exit $?
+fi
 cd integration-test
 terraform destroy --auto-approve
+if [ $? -eq 0 ]
+then
+  echo "Successfully destroyed resources"
+else
+  echo "Failed to destroy resources" >&2
+  exit $?
+fi
