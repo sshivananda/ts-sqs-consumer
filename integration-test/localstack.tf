aa8afdd5c8098f19de2b8bcdbf0196649e5487c2
Shivananda Sahu <sshivananda@athenahealth.com>
2020-05-31 19:47:38 +0530

    Creating DLQ in local stack

--- integration-test/localstack.tf
+++ integration-test/localstack.tf
@@ -4,12 +4,22 @@ provider "aws" {
   secret_key                  = "fake"
   skip_requesting_account_id  = true
   skip_credentials_validation = true
+  version                     = "~> 2.64"
 
   endpoints {
     sqs = "http://localhost:4566"
   }
 }
 
+resource "aws_sqs_queue" "test_queue_dlq" {
+  name                              = "test_queue_dlq"
+  delay_seconds                     = 1
+  max_message_size                  = 2048
+  message_retention_seconds         = 86400
+  receive_wait_time_seconds         = 10
+  kms_master_key_id                 = "alias/aws/sqs"
+  kms_data_key_reuse_period_seconds = 300
+}
 resource "aws_sqs_queue" "test_queue" {
   name                              = "test_queue"
   delay_seconds                     = 1
@@ -18,4 +28,8 @@ resource "aws_sqs_queue" "test_queue" {
   receive_wait_time_seconds         = 10
   kms_master_key_id                 = "alias/aws/sqs"
   kms_data_key_reuse_period_seconds = 300
+  redrive_policy                    = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.test_queue_dlq.arn
+    maxReceiveCount     = 4
+  })
 }
