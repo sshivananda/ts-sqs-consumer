1aa7a8b1297db4ccc507d3b2039032400f0ae8ba
Shivananda Sahu <sshivananda@athenahealth.com>
2020-05-31 17:32:07 +0530

    Exiting with error code

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -5,7 +5,7 @@ then
   echo "Successfully started docker"
 else
   echo "Could not start docker" >&2
-  exit $?
+  exit 1
 fi
 terraform init
 if [ $? -eq 0 ]
@@ -13,7 +13,7 @@ then
   echo "Successfully started terraform"
 else
   echo "Could not start terraform" >&2
-  exit $?
+  exit 1
 fi
 terraform apply -auto-approve
 if [ $? -eq 0 ]
@@ -21,7 +21,7 @@ then
   echo "Successfully created resources"
 else
   echo "Could not create resources" >&2
-  exit $?
+  exit 1
 fi
 cd ..
 npm run integration-test
@@ -30,7 +30,7 @@ then
   echo "Successfully ran integration test"
 else
   echo "Failed integration test" >&2
-  exit $?
+  exit 1
 fi
 cd integration-test
 terraform destroy --auto-approve
@@ -39,5 +39,5 @@ then
   echo "Successfully destroyed resources"
 else
   echo "Failed to destroy resources" >&2
-  exit $?
+  exit 1
 fi
